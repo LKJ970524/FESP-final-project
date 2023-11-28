02769b58cc90bdda8b161d2550c360ee529c780b
LKJ970524 <dlrbwjd7096@naver.com>
2023-11-28 14:15:02 +0900

    ♻️ refactor: spinner 적용코드 refactor (#54)
    spinner 기능을 수정하였습니다.

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,47 +1,33 @@
-import React, { Suspense } from 'react'
 import {
   CommonFooter,
   CommonHeader,
-  Spinner,
-  CommonTopButton
+  MainContent,
+  MainThumbnail,
+  Spinner
 } from '@components'
+import { CommonTopButton } from '@common'
 import { useLoaderData } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { isProduction } from '@utils'
 import { getMainData } from '@main'
 
-// React.lazy 함수는 동적으로 컴포넌트를 로드하기 위해 사용됩니다.
-const MainThumbnail = React.lazy(() =>
-  import('@components/main/MainThumbnail').then(module => ({
-    default: module.MainThumbnail
-  }))
-)
-const MainContent = React.lazy(() =>
-  import('@components/main/MainContent').then(module => ({
-    default: module.MainContent
-  }))
-)
-
 export const MainPage = () => {
-  const { data: mainQuery } = useQuery({
+  const { data: mainQuery, isLoading } = useQuery({
     queryKey: ['mainPage-data'],
     queryFn: getMainData
   })
 
-  // Suspense 컴포넌트는 fallback prop을 통해 로딩 중에 표시할 대체 콘텐츠를 설정합니다. 이 경우, fallback으로는 Spinner 컴포넌트를 설정했습니다.
+  if (isLoading) return <Spinner />
+
+  const preLoadData: any = isProduction ? mainQuery.items : useLoaderData()
+
   return (
-    <Suspense fallback={<Spinner />}>
-      <>
-        <CommonHeader />
-        <MainThumbnail
-          preLoadData={isProduction ? mainQuery.items : useLoaderData()}
-        />
-        <MainContent
-          preLoadData={isProduction ? mainQuery.items : useLoaderData()}
-        />
-        <CommonFooter />
-        <CommonTopButton />
-      </>
-    </Suspense>
+    <>
+      <CommonHeader />
+      <MainThumbnail preLoadData={preLoadData} />
+      <MainContent preLoadData={preLoadData} />
+      <CommonFooter />
+      <CommonTopButton />
+    </>
   )
 }
