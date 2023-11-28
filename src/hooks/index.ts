526b58e9093615c4c26d85b668425db1ef7492a9
LKJ970524 <dlrbwjd7096@naver.com>
2023-11-28 11:26:21 +0900

    ✨feat: imagelazyload 기능 (#54)
    imagelazyload hook을 추가하였습니다.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
 export * from './useSwipe'
 export * from './useDebounce'
 export * from './useModal'
+export * from './useIsElementInViewport'
+export * from './useIsImgLoaded'
---
+++ src/hooks/useIsImgLoaded.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import { useIsElementInViewport } from '@hooks/useIsElementInViewport'
+
+export const useIsImgLoaded = (lazy: boolean) => {
+  const { elementRef, isVisible } = useIsElementInViewport({
+    rootMargin: '0px 0px 500px 0px'
+  })
+  const [isLoaded, setIsLoaded] = useState(!lazy)
+
+  useEffect(() => {
+    if (isLoaded || !isVisible) {
+      return
+    }
+
+    setIsLoaded(true)
+  }, [isVisible])
+
+  return { elementRef, isLoaded }
+}
